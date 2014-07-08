0bddf24a06f3962dad17978327ca96fc9e5a6b17
Ivan Wolf <iwolf@detailag.ch>
2014-07-08 16:01:11 +0200

    Added module for Gaufrette (without function yet)

---
+++ Module.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Detail\Gaufrette;
+
+use Zend\ModuleManager\Feature\AutoloaderProviderInterface;
+use Zend\ModuleManager\Feature\ConfigProviderInterface;
+use Zend\ModuleManager\Feature\ControllerProviderInterface;
+use Zend\ModuleManager\Feature\ServiceProviderInterface;
+
+class Module implements
+    AutoloaderProviderInterface,
+    ConfigProviderInterface,
+    ControllerProviderInterface,
+    ServiceProviderInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getAutoloaderConfig()
+    {
+        return array(
+            'Zend\Loader\ClassMapAutoloader' => array(
+                __DIR__ . '/autoload_classmap.php',
+            ),
+            'Zend\Loader\StandardAutoloader' => array(
+                'namespaces' => array(
+                    __NAMESPACE__ => __DIR__ . '/src/' . str_replace("\\", "/", __NAMESPACE__),
+                ),
+            ),
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getConfig()
+    {
+        return include __DIR__ . '/config/module.config.php';
+    }
+
+    public function getControllerConfig()
+    {
+        return array();
+    }
+
+    public function getServiceConfig()
+    {
+        return array();
+    }
+}
---
+++ config/module.config.php
@@ -0,0 +1,20 @@
+<?php
+
+return array(
+    'service_manager' => array(
+        'abstract_factories' => array(
+        ),
+        'aliases' => array(
+        ),
+        'invokables' => array(
+        ),
+        'factories' => array(
+        ),
+        'initializers' => array(
+        ),
+        'shared' => array(
+        ),
+    ),
+    'gaufrette' => array(
+    ),
+);

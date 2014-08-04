989d3f7f4080e9333931b79c84adbc8bef27a1e8
Ivan Wolf <iwolf@detailag.ch>
2014-08-04 16:52:04 +0200

    Implemented proper factories for Detail\File storages (ZFSKELETON-3)

--- src/Detail/Gaufrette/Options/AdapterOptions.php
+++ src/Detail/Gaufrette/Options/AdapterOptions.php
@@ -3,48 +3,9 @@
 namespace Detail\Gaufrette\Options;
 
 use Detail\Core\Options\AbstractOptions;
+use Detail\Core\Options\TypeAwareOptionsTrait;
 
 class AdapterOptions extends AbstractOptions
 {
-    /**
-     * @var string
-     */
-    protected $type;
-
-    /**
-     * @var array
-     */
-    protected $options = array();
-
-    /**
-     * @return string
-     */
-    public function getType()
-    {
-        return $this->type;
-    }
-
-    /**
-     * @param string $type
-     */
-    public function setType($type)
-    {
-        $this->type = $type;
-    }
-
-    /**
-     * @return array
-     */
-    public function getOptions()
-    {
-        return $this->options;
-    }
-
-    /**
-     * @param array $options
-     */
-    public function setOptions(array $options)
-    {
-        $this->options = $options;
-    }
+    use TypeAwareOptionsTrait;
 }
